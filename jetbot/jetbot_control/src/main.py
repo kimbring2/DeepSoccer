767f93481c4d01a5438644e6b541a26d28dd3e7f
Dohyeong Kim <kimbring2@gmail.com>
2019-11-03 22:03:39 +0900

    Add files via upload
    Multiple robot python code

--- jetbot/jetbot_control/src/main.py
+++ jetbot/jetbot_control/src/main.py
@@ -3,6 +3,7 @@ import rospy
 from gazebo_msgs.msg import ModelStates
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Point
@@ -11,22 +12,16 @@ from math import atan2
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import math
- 
+
 #start is x:0, y:0
 x = 0.0
 y = 0.0
 theta = 0.0     #current angle of robot
- 
+
 bridge = CvBridge()
-cv2.namedWindow("Image Window", 1)
+#cv2.namedWindow("Image Window", 1)
 
-
-def show_image(img):
-    cv2.imshow("Image Window", img)
-    cv2.waitKey(3)
-
-
-def image_callback(msg):
+def image_callback_1(msg):
     # log some info about the image topic
     rospy.loginfo(msg.header)
 
@@ -37,8 +32,27 @@ def image_callback(msg):
         rospy.logerr("CvBridge Error: {0}".format(e))
 
     # Show the converted image
-    show_image(cv_image)
+    cv2.imshow("Robot1 Image Window", cv_image)
+    cv2.waitKey(3)
 
+
+def image_callback_2(msg):
+    # log some info about the image topic
+    rospy.loginfo(msg.header)
+
+    # Try to convert the ROS Image message to a CV2 Image
+    try:
+        cv_image = bridge.imgmsg_to_cv2(msg, "passthrough")
+    except CvBridgeError, e:
+        rospy.logerr("CvBridge Error: {0}".format(e))
+
+    # Show the converted image
+    cv2.imshow("Robot2 Image Window", cv_image)
+    cv2.waitKey(3)
+
+
+# maybe do some 'wait for service' here
+reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
 
 def state_callback(msg):
     # print("msg.name: " + str(msg.name))
@@ -50,13 +64,19 @@ def state_callback(msg):
     #print("football_index: " + str(football_index))
 
     football_pose = (msg.pose)[football_index]
-    print("football_pose: " + str(football_pose))
+    #print("football_pose.position.x: " + str(football_pose.position.x))
+
+    football_pose_x = football_pose.position.x
+    print("football_pose_x: " + str(football_pose_x))
+
+    if (football_pose_x > 5):
+        reset_simulation()
 
     left_goal_pose = (msg.pose)[left_goal_index]
-    print("left_goal_pose: " + str(left_goal_pose))
+    #print("left_goal_pose: " + str(left_goal_pose))
 
     right_goal_pose = (msg.pose)[right_goal_index]
-    print("right_goal_pose: " + str(right_goal_pose))
+    #print("right_goal_pose: " + str(right_goal_pose))
 
     global x
     global y
@@ -69,17 +89,21 @@ def state_callback(msg):
  
  
 rospy.init_node('jetbot')
-sub_image = rospy.Subscriber("/jetbot/camera1/image_raw", Image, image_callback)
+sub_image_1 = rospy.Subscriber("/robot1/camera1/image_raw", Image, image_callback_1)
+sub_image_2 = rospy.Subscriber("/robot2/camera1/image_raw", Image, image_callback_2)
 sub_state = rospy.Subscriber('/gazebo/model_states', ModelStates, state_callback)
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
-pub_vel_1 = rospy.Publisher('/jetbot/joint1_velocity_controller/command', Float64, queue_size=10)
-pub_vel_2 = rospy.Publisher('/jetbot/joint2_velocity_controller/command', Float64, queue_size=10)
+pub_vel_left_1 = rospy.Publisher('/robot1/joint1_velocity_controller/command', Float64, queue_size=10)
+pub_vel_right_1 = rospy.Publisher('/robot1/joint2_velocity_controller/command', Float64, queue_size=10)
+
+pub_vel_left_2 = rospy.Publisher('/robot2/joint1_velocity_controller/command', Float64, queue_size=10)
+pub_vel_right_2 = rospy.Publisher('/robot2/joint2_velocity_controller/command', Float64, queue_size=10)
  
 speed = Twist()
- 
-r = rospy.Rate(4)
- 
+
+r = rospy.Rate(10)
+
 goal = Point()
 goal.x = -2
 goal.y = -1
@@ -100,8 +124,11 @@ while not rospy.is_shutdown():
     velocity_2 = 0
     #rospy.loginfo(velocity_1)
     #rospy.loginfo(velocity_2)
-    pub_vel_1.publish(velocity_1)
-    pub_vel_2.publish(velocity_2)
+    pub_vel_left_1.publish(20)
+    pub_vel_right_1.publish(20)
+
+    pub_vel_left_2.publish(20)
+    pub_vel_right_2.publish(20)
     #pub.publish(speed)
  
 r.sleep()
