8695a50721e0d71638a5e90eff91820274a97ff6
Dohyeong Kim <kimbring2@gmail.com>
2020-07-18 06:48:29 +0900

    Add files via upload
    Prepare to implement Forgetful Experience Replay in Hierarchical Reinforcement Learning from Demonstrations

--- jetbot/jetbot_control/src/main_soccer.py
+++ jetbot/jetbot_control/src/main_soccer.py
@@ -1,4 +1,14 @@
 #! /usr/bin/env python
+import math
+import numpy as np
+import random
+import time
+import itertools
+import os
+from collections import deque
+from os import listdir
+from os.path import isfile, join, isdir
+
 ############## ROS Import ###############
 import rospy
 from gazebo_msgs.msg import ModelStates
@@ -7,38 +17,81 @@ from gazebo_msgs.srv import SetModelState
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float64
 from std_srvs.srv import Empty
-from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Point
 from geometry_msgs.msg import Pose
+
+############## Image Import ###############
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
-import cvlib as cv
-from cvlib.object_detection import draw_bbox
-import math
-from math import atan2
-import numpy as np
-import random
-import time
-import itertools
-import os
 
-
-# maybe do some 'wait for service' here
-reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+############## Deep Learning Import ###############
+import tensorflow as tf
+import per_replay as replay
 
 ############## ROS Part ###############
-rospy.init_node('jetbot')
+reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+rospy.init_node('jetbot_soccer')
+bridge = CvBridge()
+
+
+def image_callback(msg):
+    # log some info about the image topic
+    #rospy.loginfo(msg.header)
+
+    # Try to convert the ROS Image message to a CV2 Image
+    try:
+        cv_image = bridge.imgmsg_to_cv2(msg, "passthrough")
+    except CvBridgeError, e:
+        rospy.logerr("CvBridge Error: {0}".format(e))
+
+    new_cv_image = cv2.resize(cv_image, (512, 512), interpolation=cv2.INTER_AREA)
+
+    cv2.imshow("Image of Robot Camera", new_cv_image)
+    cv2.waitKey(3)
+
+
+def state_callback(msg):
+    #print("msg.name: " + str(msg.name))
+    # msg.name: ['ground_plane', 'field', 'left_goal', 'right_goal', 'football', 'jetbot']
+
+    robot1_index = (msg.name).index("robot1")
+    football_index = (msg.name).index("football")
+    left_goal_index = (msg.name).index("left_goal")
+    right_goal_index = (msg.name).index("right_goal")
+    #print("football_index: " + str(football_index))
+
+    robot1_pose = (msg.pose)[robot1_index]
+    #print("robot1_pose.position: " + str(robot1_pose.position))
+
+    football_pose = (msg.pose)[football_index]
+    #print("football_pose.position: " + str(football_pose.position))
+
+    left_goal_pose = (msg.pose)[left_goal_index]
+    #print("left_goal_pose.position: " + str(left_goal_pose.position))
+
+    right_goal_pose = (msg.pose)[right_goal_index]
+    #print("right_goal_pose.position: " + str(right_goal_pose.position))
+
+    #print("football_pose.position.x: " + str(football_pose.position.x))
+    #print("left_goal_pose.position.x: " + str(left_goal_pose.position.x))
+    #print("left_goal_pose.position.y: " + str(left_goal_pose.position.y))
+    if (football_pose.position.x < left_goal_pose.position.x):
+        d = True
+    elif (football_pose.position.x > right_goal_pose.position.x):
+        d = True
 
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-
 wheel1 = rospy.Publisher('/robot1/wheel1_velocity_controller/command', Float64, queue_size=5)
 wheel2 = rospy.Publisher('/robot1/wheel2_velocity_controller/command', Float64, queue_size=5)
 wheel3 = rospy.Publisher('/robot1/wheel3_velocity_controller/command', Float64, queue_size=5)
 wheel4 = rospy.Publisher('/robot1/wheel4_velocity_controller/command', Float64, queue_size=5)
 roller = rospy.Publisher('/robot1/roller_velocity_controller/command', Float64, queue_size=5)
 stick  = rospy.Publisher('/robot1/stick_velocity_controller/command', Float64, queue_size=5)
- 
+
+sub_image = rospy.Subscriber("/robot1/camera1/image_raw", Image, image_callback)
+sub_state = rospy.Subscriber('/gazebo/model_states', ModelStates, state_callback)
+
 rate = rospy.Rate(2000)
 
 stop_action = [0, 0, 0, 0, 30, 80]
@@ -54,8 +107,6 @@ set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 pose = Pose() 
 pose.position.x = np.random.randint(1,20) / 10.0
 pose.position.y = np.random.randint(1,20) / 10.0
-#pose.position.x = 0
-#pose.position.y = 0
 pose.position.z = 0.12
   
 pose.orientation.x = 0
@@ -67,6 +118,7 @@ state_model = ModelState()
 state_model.model_name = "football"
 state_model.pose = pose
 resp = set_state(state_model)
+
 
 ############## ROS + Deep Learning Part ###############
 while not rospy.is_shutdown():
